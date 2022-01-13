a6802de8f9b15518a18474a80f67954f3749b199
Orange2107 <659102800@qq.com>
2022-01-13 23:40:28 +0800

    Finish spider code

--- spider.py
+++ spider.py
@@ -4,17 +4,75 @@
 # @File  :   spider.py
 # @software : PyCharm
 
-import urllib.request, urllib.error #制定url，获取页面
+import json
 import re
-from bs4 import BeautifulSoup  #网页解析，获取数据
+import sqlite3
+import urllib.request, urllib.error #制定url，获取页面
+from urllib import parse
+
+import bs4
+from bs4 import  BeautifulSoup
+
+
+def deal_salary(money):
+    if money[-1] == '千':
+       money2 = money[0:-1]
+       money2 = money2.split("-")
+       low = float(money2[0])/10
+       hight = float(money2[1])/10
+       middle_salary = round((hight+low)/2, 2)
+    if money[-1] == '万':
+        money2 = money[0:-1]
+        money2 = money2.split("-")
+        low = float(money2[0])
+        hight = float(money2[1])
+        middle_salary = round((hight + low) / 2, 2)
+    return  middle_salary
+
+def deal_salary_year(money):
+    if money[-1] == '千':
+       money2 = money[0:-1]
+       money2 = money2.split("-")
+       low = float(money2[0])/10
+       hight = float(money2[1])/10
+       middle_salary = round((hight+low)/24, 2)
+    if money[-1] == '万':
+        money2 = money[0:-1]
+        money2 = money2.split("-")
+        low = float(money2[0])
+        hight = float(money2[1])
+        middle_salary = round((hight + low) /24, 2)
+    return  middle_salary
+
+def find_data(dic_data2):
+    tap = '","'
+    job_data = ""
+    salary = dic_data2["providesalary_text"]
+    if salary[-1] == '年':
+        salary = salary[0:-2]
+        middle_salary = deal_salary_year(salary)
+    else:
+        salary = salary[0:-2]
+        middle_salary = deal_salary(salary)
+    print(salary)
+    job_data = '"'+dic_data2["job_name"]+tap+dic_data2["company_name"]+tap+dic_data2["workarea_text"]+tap+dic_data2["companytype_text"]+tap+str(middle_salary)+'"'
+    print(job_data)
+    return job_data
+
 
 def getData(jobPath):
-   for page in range(1, 2):
+   for page in range(1, 13):         #搜索多个结果页面
        jobPath = jobPath + str(page)+".html"
-       html = askUrl(jobPath)
+       html = askUrl(jobPath)       #返回一个搜索结果页面
        #页面解析
-       #Page = BeautifulSoup(html, "html.parser")
-
+       bs = bs4.BeautifulSoup(html, "html.parser")
+       data = bs.find_all("script", type="text/javascript")
+       json_data = re.findall(detail_link, str(data))   #整个页面的所有工作消息都存放在这个json数据中
+       dic_data = json.loads(json_data[0])     #提取第一个元素，转换成字典
+       for key, value in dic_data.items():
+           if key == "engine_jds":  #每个工作详细数据存放在engine_jds中
+               for item in value:
+                   job_list.append(find_data(item))
 
 def askUrl(jobPath):
     head = {
@@ -33,12 +91,51 @@ def askUrl(jobPath):
 
 
 
-#def saveData(dbpath):
+def initdb(dbpath):
+    db = sqlite3.connect(dbpath)
+    flag = db.cursor()
+    sql = '''
+        create table job(
+        id integer primary key autoincrement,
+        job_name text not null,
+        company_name text not null,
+        work_area text not null,
+        company_type text not null,
+        provide_salary real not null
+        )'''
+    flag.execute(sql)
+    db.commit()
+    db.close()
+
+
+
+
+def saveData(dbpath, job_list):
+    initdb(dbpath)
+    #把每个job_list中的数据放入数据库中
+    db = sqlite3.connect("51job.db")
+    flag = db.cursor()
+    for job_item in job_list:
+        sql = '''
+        insert into job(
+        job_name,company_name,work_area,company_type,provide_salary)
+        values (%s) '''% job_item
+        flag.execute(sql)
+        print("插入成功！")
+    db.commit()
+    flag.close()
+    db.close()
 
 
 if __name__ == "__main__":   #当程序执行时干的事
     page = ""
+    dbpath = "51job.db"
+    job_list = []  # 用来存放工作的数据
+    detail_link = re.compile(r'window.__SEARCH_RESULT__ = (.*?)</script>', re.S)
     job = input("请输入你的工作类型")
+    job = parse.quote(parse.quote(job))
     jobPath = "https://search.51job.com/list/110200,000000,0000,00,9,99,"+job+",2,"
-    getData(jobPath)
-    #saveData(dbpath)
+    print(jobPath)
+    #getData(jobPath)
+
+    #saveData(dbpath, job_list)
--- test1.py
+++ test1.py
@@ -5,35 +5,111 @@
 # @software : PyCharm
 import json
 import re
+import sqlite3
 
 import bs4
 from bs4 import  BeautifulSoup
 
+def deal_salary(money):
+    if money[-1] == '千':
+       money2 = money[0:-1]
+       money2 = money2.split("-")
+       low = float(money2[0])/10
+       hight = float(money2[1])/10
+       middle_salary = round((hight+low)/2, 2)
+    if money[-1] == '万':
+        money2 = money[0:-1]
+        money2 = money2.split("-")
+        low = float(money2[0])
+        hight = float(money2[1])
+        middle_salary = round((hight + low) / 2, 2)
+    return  middle_salary
+
+def deal_salary_year(money):
+    if money[-1] == '千':
+       money2 = money[0:-1]
+       money2 = money2.split("-")
+       low = float(money2[0])/10
+       hight = float(money2[1])/10
+       middle_salary = round((hight+low)/24, 2)
+    if money[-1] == '万':
+        money2 = money[0:-1]
+        money2 = money2.split("-")
+        low = float(money2[0])
+        hight = float(money2[1])
+        middle_salary = round((hight + low) /24, 2)
+    return  middle_salary
+
 def find_data(dic_data2):
-    for key, value in dic_data2.items():
-        if key == 'job_title':
-            print(value)
+    tap = '","'
+    job_data = ""
+    salary = dic_data2["providesalary_text"]
+    if salary[-1] == '年':
+        salary = salary[0:-2]
+        middle_salary = deal_salary_year(salary)
+    else:
+        salary = salary[0:-2]
+        middle_salary = deal_salary(salary)
+    print(salary)
+    job_data = '"'+dic_data2["job_name"]+tap+dic_data2["company_name"]+tap+dic_data2["workarea_text"]+tap+dic_data2["companytype_text"]+tap+str(middle_salary)+'"'
+    print(job_data)
+    return job_data
+
+def initdb(dbpath):
+    db = sqlite3.connect(dbpath)
+    flag = db.cursor()
+    sql = '''
+        create table job(
+        id integer primary key autoincrement,
+        job_name text not null,
+        company_name text not null,
+        work_area text not null,
+        company_type text not null,
+        provide_salary real not null
+        )'''
+    flag.execute(sql)
+    db.commit()
+    db.close()
+
+
+
+
+def saveData(dbpath, job_list):
+    initdb(dbpath)
+    #把每个job_list中的数据放入数据库中
+    db = sqlite3.connect("test_job.db")
+    flag = db.cursor()
+    for job_item in job_list:
+        sql = '''
+        insert into job(
+        job_name,company_name,work_area,company_type,provide_salary)
+        values (%s) '''% job_item
+        flag.execute(sql)
+        print("插入成功！")
+    db.commit()
+    flag.close()
+    db.close()
+
+
+
+
 
 
 
 if __name__ == "__main__":   #当程序执行时干的事
-    href = []
-    name = []
+    job_list = []      #用来存放工作的数据
+    dbpath = "test_job.db"
     html = open(r"a job.html")
     bs = bs4.BeautifulSoup(html, "html.parser")
     data = bs.find_all("script", type= "text/javascript")
     detail_link = re.compile(r'window.__SEARCH_RESULT__ = (.*?)</script>', re.S)
-    json_data = re.findall(detail_link, str(data))
-    dic_data = json.loads(json_data[0])
-    #print(dic_data)
+    json_data = re.findall(detail_link, str(data))   #整个页面的所有工作消息都存放在这个json数据中
+    dic_data = json.loads(json_data[0])     #提取第一个元素，转换成字典
     for key, value in dic_data.items():
-       if key == "engine_jds":
+       if key == "engine_jds":   #每个工作详细数据存放在engine_jds中
            for item in value:
-               find_data(item)
-
-
-
-
+               job_list.append(find_data(item))
+    saveData(dbpath, job_list)
 
 
 
