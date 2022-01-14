fbaf03056f903937c1b472a774d798652652ca3b
Orange2107 <659102800@qq.com>
2022-01-14 22:50:14 +0800

    Ip proxy

--- spider.py
+++ spider.py
@@ -9,7 +9,7 @@ import re
 import sqlite3
 import urllib.request, urllib.error #制定url，获取页面
 from urllib import parse
-
+import time, random
 import bs4
 from bs4 import  BeautifulSoup
 
@@ -61,30 +61,38 @@ def find_data(dic_data2):
 
 
 def getData(jobPath):
-   for page in range(1, 13):         #搜索多个结果页面
-       jobPath = jobPath + str(page)+".html"
-       html = askUrl(jobPath)       #返回一个搜索结果页面
+   for page in range(2, 12):         #搜索多个结果页面
+       jobPath2 = jobPath + str(page)+".html"
+       print(jobPath2)
+       time.sleep(random.random() * 3)
+       html = askUrl(jobPath2)       #返回一个搜索结果页面
        #页面解析
        bs = bs4.BeautifulSoup(html, "html.parser")
        data = bs.find_all("script", type="text/javascript")
        json_data = re.findall(detail_link, str(data))   #整个页面的所有工作消息都存放在这个json数据中
-       dic_data = json.loads(json_data[0])     #提取第一个元素，转换成字典
-       for key, value in dic_data.items():
-           if key == "engine_jds":  #每个工作详细数据存放在engine_jds中
-               for item in value:
-                   job_list.append(find_data(item))
+       if len(json_data) != 0:
+           dic_data = json.loads(json_data[0])     #提取第一个元素，转换成字典
+           for key, value in dic_data.items():
+               if key == "engine_jds":  #每个工作详细数据存放在engine_jds中
+                   for item in value:
+                       job_list.append(find_data(item))
+       else:
+           print("超出页面！！！")
 
 def askUrl(jobPath):
     head = {
 
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
     }  # 告诉豆瓣服务器，我们是什么类型的浏览器
+    proxy = {'http':'111.3.118.247：30001'}
+    proxy_support = urllib.request.ProxyHandler(proxy)
+    opener = urllib.request.build_opener(proxy_support)
+    urllib.request.install_opener(opener)
     req = urllib.request.Request(url=jobPath, headers=head)  # 构造一个对象，封装浏览器信息
     html = ""  # 字符串
     try:
         response = urllib.request.urlopen(req) #构造http请求发送给服务器
         html = response.read().decode('gbk') #返回html是一个对象
-        print(html)
     except Exception as result:
         print(result)
     return html
@@ -111,7 +119,7 @@ def initdb(dbpath):
 
 
 def saveData(dbpath, job_list):
-    initdb(dbpath)
+    #initdb(dbpath)
     #把每个job_list中的数据放入数据库中
     db = sqlite3.connect("51job.db")
     flag = db.cursor()
@@ -132,10 +140,9 @@ if __name__ == "__main__":   #当程序执行时干的事
     dbpath = "51job.db"
     job_list = []  # 用来存放工作的数据
     detail_link = re.compile(r'window.__SEARCH_RESULT__ = (.*?)</script>', re.S)
-    job = input("请输入你的工作类型")
+    job = input("请输入你的工作类型: ")
     job = parse.quote(parse.quote(job))
     jobPath = "https://search.51job.com/list/110200,000000,0000,00,9,99,"+job+",2,"
-    print(jobPath)
-    #getData(jobPath)
+    getData(jobPath)
 
-    #saveData(dbpath, job_list)
+    saveData(dbpath, job_list)
